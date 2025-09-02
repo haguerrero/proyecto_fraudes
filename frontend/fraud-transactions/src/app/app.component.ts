495761c23f77648c997b6477c3ee52a182a334ad
Hugo Guerrero <hugoguerrero03@gmail.com>
2025-09-01 19:53:08 -0500

    Mapping changes after adding the pipeline Code cleaned up and documented.

--- frontend/fraud-transactions/src/app/app.component.ts
+++ frontend/fraud-transactions/src/app/app.component.ts
@@ -14,79 +14,112 @@ import { FraudServicesService } from './services/fraud-services.service';
     MatSlideToggleModule,
     PercentPipe,
     DecimalPipe,
-    NgbTooltipModule
+    NgbTooltipModule,
   ],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-
   dataPredict: {} = {};
   transactions: any[] = [];
   singleData: string = '';
-  singleResult: any = {}
-  batchResults: any = {}
+  singleResult: any = {};
+  batchResults: any = {};
   batchCount: number = 10;
   showSingleResult: boolean = false;
   showResults: boolean = false;
-  jsonExample: string = `{
-        "TransactionID": "SIM000003",
-        "step": 718,
-        "amount": 86868.3,
-        "type_CASH_OUT": false,
-        "type_DEBIT": true,
-        "type_PAYMENT": false,
-        "type_TRANSFER": false
-      }`;
 
+  /**
+   * Example JSON for single transaction input
+   */
+  jsonExample: string = `Ejemplo de Json: \n\n{
+        "TransactionID": "SIM000001",
+        "step": 484,
+        "type": "PAYMENT",
+        "amount": 12492.87,
+        "nameOrig": "C7038986",
+        "oldbalanceOrg": 12969.04,
+        "newbalanceOrg": 476.1700000000001,
+        "nameDest": "C6952866",
+        "oldbalanceDest": 0,
+        "newbalanceDest": 12492.87,
+        "isFraud": 0,
+        "isFlaggedFraud": 0 \n}`;
+
+  /**
+   * Constructor
+   * @param fraudService Service to interact with the backend API
+   * @param cd ChangeDetectorRef to manually trigger change detection
+   */
   constructor(
     private fraudService: FraudServicesService,
     private cd: ChangeDetectorRef
-  ) {
+  ) {}
 
-  }
+  /**
+   * Author: Hugo Guerrero
+   * Date: June 2024
+   * email: hugoguerrero03@mail.com
+   * Description: This Angular component interacts with a fraud detection backend API.
+   * It provides functionalities for single and batch transaction predictions,
+   * as well as generating simulated transactions. The component manages API responses,
+   * and UI toggles to display results.
+   */
 
-
+  /**
+   * Single Prediction
+   * @param data JSON string of a single transaction
+   */
   predict(data: any) {
     const parsed = JSON.parse(data);
     this.fraudService.predict(parsed).subscribe({
       next: (response) => {
-        this.singleResult = {...response};
+        this.singleResult = { ...response };
       },
       error: (error) => {
         console.error('Error al realizar la predicción!', error);
-      }
+      },
     });
   }
 
+  /**
+   * Batch Prediction
+   * @param transactions  Array of transaction objects
+   */
   predictBatch(transactions: any[]) {
-
     this.fraudService.predictBatch(transactions).subscribe({
       next: (response) => {
-        this.batchResults = {...response};
+        this.batchResults = { ...response };
         this.cd.detectChanges();
       },
       error: (error) => {
         console.error('Error al realizar la predicción por lotes!', error);
-      }
+      },
     });
   }
 
-    getTransactions(arg: number) {
+  /**
+   * generate Transactions
+   * @param arg Number of transactions to generate
+   */
+  getTransactions(arg: number) {
     this.fraudService.generateTransaction(arg).subscribe({
       next: (response) => {
-        if(response){
+        if (response) {
           this.predictBatch(response.transactions);
         }
       },
       error: (error) => {
         console.error('Error al generar las transacciones!', error);
-      }
+      },
     });
   }
 
+  /**
+   * Toggle display of json of results
+   * @param variableName Variable boolean to toggle
+   */
   showResultsToggle(variableName: 'showResults' | 'showSingleResult') {
-  this[variableName] = !this[variableName];
-}
-
+    this[variableName] = !this[variableName];
+  }
 }
